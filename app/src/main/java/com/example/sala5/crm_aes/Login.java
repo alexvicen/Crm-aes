cbbe48ef4a92979124e735c53074687e9289bbec
aes-multimedia <soporte@aes-multimedia.com>
2016-05-12 17:38:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sala5/crm_aes/Login.java
@@ -0,0 +1,13 @@
+package com.example.sala5.crm_aes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
